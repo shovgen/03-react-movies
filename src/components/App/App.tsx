36dc0a47eb93b36763dbc01718d4a1abc762c73a
Hanna Ilkiv <shov.guitar@gmail.com>
2026-05-14 18:18:25 +0200

    fix error

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import Loader from "../Loader/Loader";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
-import { fetchMovies } from "../../configObject/configObject";
+import { fetchMovies } from "../../services/configObject";
 
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import type { Movie } from "../../types/movie";
